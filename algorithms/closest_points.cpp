d6136471b0a5bf60fe6c969e009266c8a64ed0b7
0xpapercut <0xpapercut@gmail.com>
2023-12-25 23:14:20 +0100

    Add closest_points problem

---
+++ algorithms/closest_points.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+using Point = pair<int, int>;
+
+pair<Point, Point> closest_points(vector<Point>) {
+
+}
+
+int main() {
+    return 0;
+}
